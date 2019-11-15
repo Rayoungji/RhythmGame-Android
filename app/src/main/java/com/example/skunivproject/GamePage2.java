bc1eddb3900ea83c7060e190ce4d4d7ef12a7aaa
dudwl7676 <dudwl7676@naver.com>
2019-11-15 23:08:42 +0900

    Game Page2 ->비트 클래스 생성

--- app/src/main/java/com/example/skunivproject/GamePage2.java
+++ app/src/main/java/com/example/skunivproject/GamePage2.java
@@ -5,36 +5,30 @@ package com.example.skunivproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
 
-import java.util.ArrayList;
+import java.util.Random;
 
-//게임 실행화면에 대한 메인 스레드
 public class GamePage2 extends AppCompatActivity {
 
     MediaPlayer mp;
-    ImageView note1,note2,note3,note4;
-    Button btn;
+    Button btn,start;
+    ImageView note1,note2,note3,note4,note5,note6,note7,note8;
 
-    ImageView[] img_array=new ImageView[4];
-    int[] imageID={R.id.image1,R.id.image2,R.id.image3,R.id.image4};
-
-    final String TAG_ON="on";
-    final String TAG_OFF = "off";
-    int score = 0;
+    //이미지뷰 배열 선언 후 값 넣어주기 ->여러 함수들에서 사용해야 하므로 전역 변수로 선언하기
+    ImageView[] img_array=new ImageView[8];
+    int[] noteID={R.id.note1,R.id.note2,R.id.note3,R.id.note4,R.id.note5,R.id.note6,R.id.note7,R.id.note8};
+    int time=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,78 +36,103 @@ public class GamePage2 extends AppCompatActivity {
         setContentView(R.layout.activity_game_page2);
 
         btn=(Button)findViewById(R.id.exit);
-        note1=(ImageView)findViewById(R.id.image1);
-        note2=(ImageView)findViewById(R.id.image2);
-        note3=(ImageView)findViewById(R.id.image3);
-        note4=(ImageView)findViewById(R.id.image4);
+        start=(Button)findViewById(R.id.start);
+        note1=(ImageView)findViewById(R.id.note1);
+        note2=(ImageView)findViewById(R.id.note2);
+        note3=(ImageView)findViewById(R.id.note3);
+        note4=(ImageView)findViewById(R.id.note4);
+        note5=(ImageView)findViewById(R.id.note5);
+        note6=(ImageView)findViewById(R.id.note6);
+        note7=(ImageView)findViewById(R.id.note7);
+        note8=(ImageView)findViewById(R.id.note8);
 
+
+
+        //각 노트에 이미지를 넣어준다
         for(int i=0; i<img_array.length; i++){
-            img_array[i]=(ImageView)findViewById(imageID[i]);
-            img_array[i].setImageResource(R.drawable.noteroutchange);
-            img_array[i].setTag(TAG_OFF);
-
-            img_array[i].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(((ImageView)v).getTag().toString().equals(TAG_ON)){
-                        Toast.makeText(getApplicationContext()."good",Toast.LENGTH_LONG).show();
-                        score++;
-                        ((ImageView)v).setImageResource(R.drawable.noteroutchange);
-                        v.setTag(TAG_OFF);
-                    }else{
-                        Toast.makeText(getApplicationContext(),"bad",Toast.LENGTH_LONG).show();
-                        if(score<=0){
-                            score=0;
-                        }
-                    }
-                }
-            });
+            //이미지뷰배열안에 여덟개의 이미지 뷰가 들어있고 각 이미지뷰에 아이디를 연동시켜준다!!(xml에서 선언한 뷰)
+            img_array[i]=(ImageView)findViewById(noteID[i]);
+            img_array[i].setImageResource(R.drawable.note);
         }
 
         //인텐트로 음악제목 가져오기
         Intent intent=getIntent();
         int img=intent.getIntExtra("img",1);
-        String imageName= intent.getStringExtra("imgTitle");
-        String a="wish you were gay",b="mine",c="paris";
-        String d="holiday", e="itzy", f="snapping";
+        final String imageName= intent.getStringExtra("imgTitle");
+        final String a="wish you were gay",b="mine",c="paris";
+        final String d="holiday", e="itzy", f="snapping";
 
-        //가져온 제목에 맞는 음악 재생하기
-        if(a.equals(imageName)) {
-            mp= MediaPlayer.create(
-                    getApplicationContext(), R.raw.wishyouweregay);
-            mp.start();
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        }
+                start.setVisibility(View.GONE);
 
-        if(b.equals(imageName))
-        { mp= MediaPlayer.create(
-                getApplicationContext(),
-                R.raw.mine);
-            mp.start();}
 
-        if(c.equals(imageName))
-        { mp= MediaPlayer.create(
-                getApplicationContext(),
-                R.raw.paris);
-            mp.start();}
+                //가져온 제목에 맞는 음악 재생하기
+                if(a.equals(imageName)) {
+                    mp= MediaPlayer.create(
+                            getApplicationContext(), R.raw.wishyouweregay);
+                    mp.start();
+//                    time=mp.getCurrentPosition();  //밀리초단위의 현재 위치
+                    Log.d("time",String.valueOf(time));
+                                Beat[] beats = {
+                    new Beat(1000,0),
+                    new Beat(3000,1),
+                    new Beat(5000,2),
+                    new Beat(7000,3),
+                    new Beat(8000,7),
+                    new Beat(10000,6),
+                    new Beat(12000,4),
+                    new Beat(14000,5)
+            };
+                    int time=1000;
+                    while(time<14000) {
+                        time =time+2000;
+                        for (int i = 0; i <=7; i++) {
+                            if (beats[i].getTime() <=time) {
+                                new Thread(new NoteThread(i)).start();
+                            }
+                        }
+                    }
 
-        if(d.equals(imageName)) {
-            mp= MediaPlayer.create(
-                    getApplicationContext(), R.raw.holiday);
-            mp.start();
-        }
 
-        if(e.equals(imageName))
-        { mp= MediaPlayer.create(
-                getApplicationContext(),
-                R.raw.itzy);
-            mp.start();}
 
-        if(f.equals(imageName))
-        { mp= MediaPlayer.create(
-                getApplicationContext(),
-                R.raw.snapping);
-            mp.start();}
+
+                }
+
+                if(b.equals(imageName))
+                { mp= MediaPlayer.create(
+                        getApplicationContext(),
+                        R.raw.mine);
+                    mp.start();}
+
+                if(c.equals(imageName))
+                { mp= MediaPlayer.create(
+                        getApplicationContext(),
+                        R.raw.paris);
+                    mp.start();}
+
+                if(d.equals(imageName)) {
+                    mp= MediaPlayer.create(
+                            getApplicationContext(), R.raw.holiday);
+                    mp.start();
+                }
+
+                if(e.equals(imageName))
+                { mp= MediaPlayer.create(
+                        getApplicationContext(),
+                        R.raw.itzy);
+                    mp.start();}
+
+                if(f.equals(imageName))
+                { mp= MediaPlayer.create(
+                        getApplicationContext(),
+                        R.raw.snapping);
+                    mp.start();}
+
+            }
+        });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -135,6 +154,63 @@ public class GamePage2 extends AppCompatActivity {
 
     }
 
+    //그림을 갈색으로 바꿔라!!
+    @SuppressLint("HandlerLeak")
+    Handler NoteHandler=new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+           // int time=new Random().nextInt(6000)+500;
+                img_array[msg.arg1].setY(img_array[msg.arg1].getY() + 7);
+        }
+    };
+
+
+    //라우터 생상 변경시키는 쓰레드
+    public class NoteThread implements Runnable{
+
+        int index=0; //노트 이름
+        NoteThread(int index){
+            this.index=index;
+        }
+
+        @Override
+        public void run() {
+        while (true){
+            try{
+                Message msg1=new Message();
+                msg1.arg1=index;
+                NoteHandler.sendMessage(msg1);
+                Thread.sleep(100);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        }
+    }
+
+    public class Beat{
+        private int time;
+        private int noteName;
+
+        Beat(int time,int noteName){
+            this.time=time;
+            this.noteName=noteName;
+        }
+        public int getTime(){
+            return time;
+        }
+        public void setTime(int time){
+            this.time=time;
+        }
+        public int getNoteName(){
+            return noteName;
+        }
+        public void setNoteName(int noteName){
+            this.noteName=noteName;
+        }
+    }
 }
 
 
--- app/src/main/java/com/example/skunivproject/SecondMain.java
+++ app/src/main/java/com/example/skunivproject/SecondMain.java
@@ -10,7 +10,6 @@ import android.widget.Button;
 public class SecondMain extends AppCompatActivity {
 
     Button Start,Rank,Logout;
-    Button practice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +19,6 @@ public class SecondMain extends AppCompatActivity {
         Start=(Button)findViewById(R.id.Start);
         Rank=(Button)findViewById(R.id.Rank);
         Logout=(Button)findViewById(R.id.Logout);
-
-        practice=(Button)findViewById(R.id.practice);
 
         Start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,14 +41,6 @@ public class SecondMain extends AppCompatActivity {
             public void onClick(View v) {
                 Intent Logout=new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(Logout);
-            }
-        });
-
-        practice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent practice=new Intent(getApplicationContext(),practice.class);
-                startActivity(practice);
             }
         });
     }
